740d7e3b40014419f7716fb62222b72a26273d00
Joshua Bradford <jjyet@yahoo.com>
2017-03-27 16:23:27 -0400

    added hasshed and jwt

--- server/controllers/UsersController.js
+++ server/controllers/UsersController.js
@@ -1,20 +1,43 @@
 const Users = require("../models").Users
+const bcrypt = require("bcryptjs");
+const jwt = require("jwt-simple");
+const appSecrets = require("../config/secret");
 
 module.exports = {
-  create (req, res) {
-   //res.status(200).send("Hello world!");
+  // create (req, res) {
+  //  //res.status(200).send("Hello world!");
+  //     Users.create({
+  //     userName: req.body.userName,
+  //     hashedPassword: req.body.hashedPassword,
+  //     salt: req.body.salt,
+  //     profileUrl: req.body.profileUrl,
+  //     backgroundUrl: req.body.backgroundUrl,
+  //     firstName: req.body.firstName,
+  //     lastName: req.body.lastName,
+  //     email: req.body.email,
+  //     city: req.body.city,
+  //   })
+  //   .then(users => res.status(201).send(users))
+  //   .catch(error => res.status(400).send(error));
+  // },
+
+
+create (req, res) {
+    var salt = bcrypt.genSaltSync(10);
+    var hashedPass = bcrypt.hashSync(req.body.hashedPassword, salt);
       Users.create({
       userName: req.body.userName,
-      hashedPassword: req.body.hashedPassword,
-      salt: req.body.salt,
+      email: req.body.email,
       profileUrl: req.body.profileUrl,
       backgroundUrl: req.body.backgroundUrl,
       firstName: req.body.firstName,
       lastName: req.body.lastName,
-      email: req.body.email,
       city: req.body.city,
+      hashedPassword: hashedPass,
+      salt: salt,
+      //user_password: req.body.user_password,
     })
     .then(users => res.status(201).send(users))
     .catch(error => res.status(400).send(error));
   },
-}
+};
