26e7261f02824d4af90de48be046969e4a91013c
Joshua Bradford <jjyet@yahoo.com>
2017-03-31 16:21:07 -0400

    added put to cors

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,7 @@ const middleware = require("../middleware");
 module.exports = (app) => {
   app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-token");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-token, PUT");
   next();
 });
   // app.get('/reviews', ReviewsController.index)
