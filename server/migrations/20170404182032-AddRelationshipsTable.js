fcab5d8e0d661f2d82c0ff66522e335b0457e273
Joshua Bradford <jjyet@yahoo.com>
2017-04-04 14:25:38 -0400

    added Relationships table

---
+++ server/migrations/20170404182032-AddRelationshipsTable.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('Relationships', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      followedId: {
+        type: Sequelize.INTEGER
+      },
+      followerId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Relationships');
+  }
+};
