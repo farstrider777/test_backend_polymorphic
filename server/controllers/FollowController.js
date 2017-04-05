75750524de1b7684b70ada11ed4f21937323886d
Joshua Bradford <jjyet@yahoo.com>
2017-04-05 09:09:06 -0400

    fix comma

--- server/controllers/FollowController.js
+++ server/controllers/FollowController.js
@@ -4,7 +4,7 @@ const Relationships = require("../models").Relationships;
 module.exports = {
   postFollow (req, res){
     Relationships.create({
-      followerId: req.user.id
+      followerId: req.user.id,
       followedId: req.params.userId
     }).then(reviews => res.status(201).send(reviews))
     .catch(error => console.log(error))
