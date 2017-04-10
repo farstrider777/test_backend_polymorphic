38f926e3dcaed940cb3a8a1581689b8ab61f1620
Joshua Bradford <jjyet@yahoo.com>
2017-04-10 08:53:39 -0400

    added patch and options to cors

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,7 +11,7 @@ const middleware = require("../middleware");
 
 module.exports = (app) => {
   app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-token");
   next();
